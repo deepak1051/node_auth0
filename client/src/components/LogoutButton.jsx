40c29d50da1ef85861a78db85ca4d5f255f752f9
deepak1051 <deepakpanwar1051@gmail.com>
2025-04-05 01:02:22 +0530

    change the client side logout auth redirect

--- client/src/components/LogoutButton.jsx
+++ client/src/components/LogoutButton.jsx
@@ -4,7 +4,12 @@ const LogoutButton = () => {
   const { logout } = useAuth0();
   return (
     <button
-      onClick={() => logout({ returnTo: window.location.origin })}
+      onClick={() =>
+        logout({
+          returnTo:
+            import.meta.env.VITE_AUTH0_LOGOUT_URL || window.location.origin,
+        })
+      }
       className="cursor-pointer bg-red-500 text-white px-4 py-2 rounded"
     >
       Logout
